71e56024d9c08d5adf82635a72395deefbb49819
Andrea <andrearivas4cinfor@gmail.com>
2022-11-21 18:51:05 +0100

    Add CSS style to menu-window

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 const windowModal = document.querySelector(".mobile-nav");
 const btnOpenMenu = document.querySelector(".hamburger-icon");
 const btnCloseMenu = document.querySelector(".btn-close");
-const modalContainer = document.querySelector('.modal-container');
-const sectionLink = document.querySelector('.link');
-const containerProject = document.querySelector(".projects-cards");
+
 
 //OPEN AND CLOSE MENU WINDOW
