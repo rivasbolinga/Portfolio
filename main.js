d2b57a60aa1af57e62694f4f652656bffa1602a3
Andrea <andrearivas4cinfor@gmail.com>
2022-11-21 18:40:26 +0100

    Add js file

---
+++ main.js
@@ -0,0 +1,10 @@
+'use script'
+
+const windowModal = document.querySelector(".mobile-nav");
+const btnOpenMenu = document.querySelector(".hamburger-icon");
+const btnCloseMenu = document.querySelector(".btn-close");
+const modalContainer = document.querySelector('.modal-container');
+const sectionLink = document.querySelector('.link');
+const containerProject = document.querySelector(".projects-cards");
+
+//OPEN AND CLOSE MENU WINDOW
