dc65b5244facc7acde84a6faa60f64f04370c0ed
Andrea <andrearivas4cinfor@gmail.com>
2022-11-21 20:57:41 +0100

    Fix linters

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-'use script'
+'use script';
 
-const windowModal = document.querySelector(".mobile-nav");
-const btnOpenMenu = document.querySelector(".hamburger-icon");
-const btnCloseMenu = document.querySelector(".btn-close");
+const windowModal = document.querySelector('.mobile-nav');
+const btnOpenMenu = document.querySelector('.hamburger-icon');
+const btnCloseMenu = document.querySelector('.btn-close');
 const sectionLink = document.querySelector('.link');
 
-//OPEN AND CLOSE MENU WINDOW
-const openMenu = function (e) {
-  windowModal.classList.remove("hidden");
-}
-const closeMenu = function (e) {
-  windowModal.classList.add("hidden");
-}
-btnOpenMenu.addEventListener("click", openMenu);
-btnCloseMenu.addEventListener("click", closeMenu);
-sectionLink.addEventListener("click",closeMenu);
+// OPEN AND CLOSE MENU WINDOW
+const openMenu = function () {
+  windowModal.classList.remove('hidden');
+};
+const closeMenu = function () {
+  windowModal.classList.add('hidden');
+};
+btnOpenMenu.addEventListener('click', openMenu);
+btnCloseMenu.addEventListener('click', closeMenu);
+sectionLink.addEventListener('click', closeMenu);
